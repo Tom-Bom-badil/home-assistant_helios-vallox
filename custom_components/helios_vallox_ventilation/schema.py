7436b6fa4044aeebbd7836cf981384710b5c12ab
Tom Bombadil <tommy_bombadil@hotmail.com>
2024-12-17 18:46:07 +0100

    create schema.py

---
+++ custom_components/helios_vallox_ventilation/schema.py
@@ -0,0 +1,68 @@
+import voluptuous as vol
+from homeassistant.const import CONF_IP_ADDRESS, CONF_PORT
+from homeassistant.helpers import config_validation as cv
+
+DOMAIN = "helios_vallox_ventilation"
+
+# Configuration schema
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: vol.Schema(
+            {
+                vol.Required(CONF_IP_ADDRESS): cv.string,
+                vol.Required(CONF_PORT): cv.port,
+                vol.Optional("sensors", default=[]): vol.All(
+                    cv.ensure_list,
+                    [
+                        vol.Schema(
+                            {
+                                vol.Required("name"): cv.string,
+                                vol.Optional("friendly_name"): cv.string,
+                                vol.Optional("unit_of_measurement"): cv.string,
+                                vol.Optional("device_class"): cv.string,
+                                vol.Optional("state_class"): cv.string,
+                                vol.Optional("min_value"): vol.Coerce(float),
+                                vol.Optional("max_value"): vol.Coerce(float),
+                                vol.Optional("default_value"): vol.Coerce(float),
+                                vol.Optional("icon"): cv.icon,
+                            }
+                        )
+                    ],
+                ),
+                vol.Optional("binary_sensors", default=[]): vol.All(
+                    cv.ensure_list,
+                    [
+                        vol.Schema(
+                            {
+                                vol.Required("name"): cv.string,
+                                vol.Optional("friendly_name"): cv.string,
+                                vol.Optional("device_class"): cv.string,
+                                vol.Optional("icon"): cv.icon,
+                            }
+                        )
+                    ],
+                ),
+                vol.Optional("switches", default=[]): vol.All(
+                    cv.ensure_list,
+                    [
+                        vol.Schema(
+                            {
+                                vol.Required("name"): cv.string,
+                                vol.Optional("friendly_name"): cv.string,
+                                vol.Optional("device_class"): cv.string,
+                                vol.Optional("icon"): cv.icon,
+                            }
+                        )
+                    ],
+                ),
+            }
+        )
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+# Service schema
+SERVICE_WRITE_VALUE_SCHEMA = vol.Schema({
+    vol.Required("variable"): cv.string,
+    vol.Required("value"): vol.Coerce(int),
+})
