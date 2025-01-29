06becc7fb5a88f68e9f7d0e4e580cce11fd69cb3
Tom Bombadil <tommy_bombadil@hotmail.com>
2025-01-29 18:27:49 +0100

    adjustments for data coordinator

--- custom_components/helios_vallox_ventilation/switch.py
+++ custom_components/helios_vallox_ventilation/switch.py
@@ -1,74 +1,92 @@
-from homeassistant.components.switch import SwitchEntity
-from custom_components.helios_vallox_ventilation import HELIOS_DATA
 import logging
-
-
+from homeassistant.components.switch import SwitchEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from .const import DOMAIN
 _LOGGER = logging.getLogger(__name__)
 
 
-# set up switches
+# platform setup
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    if discovery_info is None:
+        return
 
-    data_provider = HELIOS_DATA
-    data_provider.update()
-
-    switch_config = discovery_info.get("switches", []) if discovery_info else []
-    switches = []
+    coordinator = hass.data[DOMAIN]["coordinator"]
+    entities = []
+    switch_config = discovery_info.get("switches", [])
 
     for switch in switch_config:
         name = switch.get("name")
         if not name:
+            _LOGGER.warning("Switch configuration missing 'name'. Skipping entry.")
             continue
-        switches.append(
+        entities.append(
             HeliosSwitch(
                 name=name,
                 variable=name,
-                data_provider=data_provider,
-                device_class=switch.get("device_class"),
-                icon=switch.get("icon")
+                coordinator=coordinator,
+                description=switch.get("description"),
+                icon=switch.get("icon"),
+                unique_id=f"ventilation_{name}",
             )
         )
-    async_add_entities(switches)
-    hass.data.setdefault("ventilation_entities", []).extend(switches)
-    _LOGGER.debug("Ventilation switches successfully set up.")
+
+    async_add_entities(entities)
+    hass.data.setdefault("ventilation_entities", []).extend(entities)
+    _LOGGER.debug(f"Added {len(entities)} switches for Helios/Vallox and registered it with coordinator {coordinator.coordinator}.")
 
 
-# representation of a switch
-class HeliosSwitch(SwitchEntity):
-
-    def __init__(self, name, variable, data_provider, device_class=None, icon=None):
-        self._name = f"ventilation_{name}"
+# switch class
+class HeliosSwitch(CoordinatorEntity, SwitchEntity):
+    def __init__(self, name, variable, coordinator, description=None, icon=None, unique_id=None):
+        super().__init__(coordinator.coordinator)
+        self._attr_name = f"Ventilation {name}"
         self._variable = variable
-        self._state = False
-        self._device_class = device_class
-        self._icon = icon
-        self._data_provider = data_provider
-
-    @property
-    def name(self):
-        return self._name
+        self._coordinator = coordinator
+        self._description = description
+        self._attr_icon = icon
+        self._attr_unique_id = unique_id
+        self._attr_is_on = None
+        # _LOGGER.debug(f"Registering switch '{name}' with coordinator: {coordinator.coordinator}")
 
     @property
     def is_on(self):
-        return bool(self._data_provider.get_value(self._variable))
+        value = self.coordinator.data.get(self._variable)
+        return value == "on" or value is True
 
+    # additional state attributes
     @property
-    def device_class(self):
-        return self._device_class
+    def extra_state_attributes(self):
+        return {
+            "description": self._description,
+        }
 
-    @property
-    def icon(self):
-        return self._icon
+    # turn on
+    async def async_turn_on(self, **kwargs):
+        await self._coordinator.turn_on(self._variable)
+        self._attr_is_on = True
+        self.async_write_ha_state()
 
-    # logic for activation
-    def turn_on(self, **kwargs):
-        _LOGGER.info(f"Turning on {self._name}")
-        self._state = True
+    # turn off
+    async def async_turn_off(self, **kwargs):
+        await self._coordinator.turn_off(self._variable)
+        self._attr_is_on = False
+        self.async_write_ha_state()
 
-    # logic for de-activation
-    def turn_off(self, **kwargs):
-        _LOGGER.info(f"Turning off {self._name}")
-        self._state = False
+    # update
+    async def async_update(self):
+        await self.coordinator.async_request_refresh()
 
-    def update(self):
-        self._state = bool(self._data_provider.get_value(self._variable))
+    # add entity
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+        self.async_write_ha_state()
+        self.async_on_remove(
+            self.coordinator.async_add_listener(self._handle_coordinator_update)
+        )
+
+    # update entity
+    def _handle_coordinator_update(self):
+        new_value = self.coordinator.data.get(self._variable)
+        if new_value is not None:
+            self._attr_is_on = new_value == "on" or new_value is True
+        self.async_write_ha_state()
